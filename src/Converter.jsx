5bcb348c50d3700565a90665033e223d3649f4fc
Marko Melle <markmelle1@gmail.com>
2023-05-04 16:58:51 +0200

    add Converter.jsx

---
+++ src/Converter.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from "react";
+
+const Converter = () => {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const timeOutRef = useRef(null);
+
+  const handleChange = (e) => {
+    clearTimeout(timeOutRef.current);
+    const value = e.target.value;
+    setInput(value);
+
+    if (value === "") {
+      setOutput("");
+    } else {
+      timeOutRef.current = setTimeout(() => {
+        if (validateInput(value)) {
+          let { r, g, b } = hexToRgb(value);
+          setOutput(`rgb(${r}, ${g}, ${b})`);
+        } else {
+          setOutput("Ошибка!");
+        }
+      }, 1000);
+    }
+  };
+
+  const validateInput = (input) => /^#([0-9a-f]{3}){1,2}$/i.test(input);
+
+  const hexToRgb = (hex) => {
+    hex = hex.replace("#", "");
+
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return { r, g, b };
+  };
+
+  const styles = {
+    backgroundColor: output,
+  };
+
+  return (
+    <div className="Converter" style={styles}>
+      <input
+        className="converter-input"
+        type="text"
+        value={input}
+        onChange={handleChange}
+      />
+      {output !== "" && <p className="converter-output">{output}</p>}
+    </div>
+  );
+};
+
+export default Converter;
